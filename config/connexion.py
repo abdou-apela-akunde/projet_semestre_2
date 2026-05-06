2b1e185690ad7abb1089b22cfd5eefc0b748164e
Abdou <abdou.apela-akunde@etu.u-pec.fr>
2026-05-06 12:29:44 +0200

    Implémentation de la fonction connexion à la base de données

---
+++ config/connexion.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
+
+def connexion():
+    load_dotenv()
+
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    host = os.getenv("DB_HOST")
+    database = os.getenv("DB_NAME")
+
+    url = f"mysql+pymysql://{user}:{password}@{host}/{database}"
+
+    engine = create_engine(url)
+
+    try:
+        with engine.connect() as connection:
+            print("Connexion réussie !")
+    except Exception as e:
+        print("Échec de la connexion.", e)
+
+    return connection
+
