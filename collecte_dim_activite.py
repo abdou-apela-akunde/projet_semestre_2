273b162d85b4761f76b1bb59dbf1e6deeba25697
yassineWasel <prenom.nom@etu.u-pec.fr>
2026-05-27 17:01:07 +0200

    Ajout de la recuperation des valeurs distinctes depuis l API

--- collecte_dim_activite.py
+++ collecte_dim_activite.py
@@ -6,3 +6,18 @@ from models_dimensions import TypeExercice, TypeSecteur
 
 
 BASE_URL = "https://data.ameli.fr/api/explore/v2.1/catalog/datasets"
+
+def recuperer_valeurs_distinctes(dataset_id, champ):
+    url = f"{BASE_URL}/{dataset_id}/records"
+
+    params = {
+        "select": champ,
+        "group_by": champ,
+        "limit": 100
+    }
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+
+    data = response.json()
+    return data.get("results", [])
