6e045b2d9cb1d615f13e9599a938e90f8d07b056
yassineWasel <prenom.nom@etu.u-pec.fr>
2026-05-27 17:01:36 +0200

    Ajout de la collecte des types d exercice

--- collecte_dim_activite.py
+++ collecte_dim_activite.py
@@ -21,3 +21,21 @@ def recuperer_valeurs_distinctes(dataset_id, champ):
 
     data = response.json()
     return data.get("results", [])
+
+def ajouter_types_exercice(session):
+    dataset_id = "demographie-exercices-liberaux"
+    champ = "libelle_type_exercice_liberal"
+
+    resultats = recuperer_valeurs_distinctes(dataset_id, champ)
+
+    for ligne in resultats:
+        libelle = ligne.get(champ)
+
+        if libelle:
+            existe = session.query(TypeExercice).filter_by(libelle=libelle).first()
+
+            if not existe:
+                session.add(TypeExercice(libelle=libelle))
+
+    session.commit()
+    print("Types d'exercice ajoutés.")
